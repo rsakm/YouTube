ee416be53d9c04983d6f6c1eb9bedb7b3ffc0bf8
Rajshree <rajshreeakm@gmail.com>
2025-05-18 20:24:50 +0530

    Updates

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,20 +1,15 @@
 import { Searchbar } from "./Searchbar"
 
-export const Appbar = ()=>{
-
-    return (
-        <div className="flex justify-between">
-
-            <div className="col-span-1">
-                YouTube
-            </div>
-            <div>
-                <Searchbar/>
-            </div>
-            <div>
-                Sign in
-            </div>
-
+export const Appbar = () => {
+    return <div className="flex justify-between	pl-3 pr-3 pt-1">
+        <div>
+            Youtube
         </div>
-    )
+        <div>
+            <Searchbar />
+        </div>
+        <div>
+            Logout
+        </div>
+    </div>
 }
--- src/components/VideoGrid.jsx
+++ src/components/VideoGrid.jsx
@@ -6,40 +6,40 @@ const videos = [
     channel:"Evergreen music",
     views:"25M views",
     timestamp:"2 years ago",
-    image:"../public/ZN_XrB_St-8-HD.jpg",
-    thumbnailImage:"../public/thumb (1).png"
+    image:"/ZN_XrB_St-8-HD.jpg",
+    thumbnailImage:"/thumb (1).png"
     },
     {
     title:"Ye Dil Tum Bin Kahin Lagta Nahin - Lata Mangeshkar, Mohammed Rafi [Slowed+Reverb]",
     channel:"Evergreen music",
     views:"21M views",
     timestamp:"1 year ago",
-    image:"../public/i2.jpg",
-    thumbnailImage:"../public/thumb (1).png"
+    image:"/i2.jpg",
+    thumbnailImage:"/thumb (1).png"
     },
     {
     title:"Achha ji main hari chalo maan jao na [Colorized] | Kala Pani (1958)",
     channel:"Evergreen music",
     views:"25M views",
     timestamp:"2 years ago",
-    image:"../public/ZN_XrB_St-8-HD.jpg",
-    thumbnailImage:"../public/thumb (1).png"
+    image:"/ZN_XrB_St-8-HD.jpg",
+    thumbnailImage:"/thumb (1).png"
     },
     {
     title:"Ye Dil Tum Bin Kahin Lagta Nahin - Lata Mangeshkar, Mohammed Rafi [Slowed+Reverb]",
     channel:"Evergreen music",
     views:"21M views",
     timestamp:"1 year ago",
-    image:"../public/i2.jpg",
-    thumbnailImage:"../public/thumb (1).png"
+    image:"/i2.jpg",
+    thumbnailImage:"/thumb (1).png"
     },
     {
     title:"Achha ji main hari chalo maan jao na [Colorized] | Kala Pani (1958)",
     channel:"Evergreen music",
     views:"25M views",
     timestamp:"2 years ago",
-    image:"../public/ZN_XrB_St-8-HD.jpg",
-    thumbnailImage:"../public/thumb (1).png"
+    image:"/ZN_XrB_St-8-HD.jpg",
+    thumbnailImage:"/thumb (1).png"
     },
 
 
